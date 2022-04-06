3390c6c2afd1f2b7f11a973ba854eeaf380d4916
Adamina Flores <adaminaflores19@gmail.com>
2022-04-06 15:29:58 -0500

    Cleaned up react files and displayed pokemon list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"])
+  const[currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const[nextPageUrl, setNextPageUrl] = useState()
+  const[prevPageUrl, setPrevPageUrl] = useState()
+
+  useEffect(()=>{
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PokemonList pokemon= {pokemon} />
   );
 }
 
